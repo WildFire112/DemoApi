bcdee081404344e4cf4323e3a54b9f90099b50ab
WildFire112 <11nbv11@gmail.com>
2019-06-27 00:11:52 +0300

    client side deleted

--- client/src/store/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { errorsReducer } from "./errors/reducers";
-import { userReducer } from './user/reducers'
-
-
-export default combineReducers({
-  user: userReducer,
-  errors: errorsReducer
-})
