c343333424c9f4f722f9cc4634a3a2802868faa9
WildFire112 <11nbv11@gmail.com>
2019-06-27 04:27:42 +0300

    cleanUp

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { sendHeaderImage } from '../store/user/actions';
-import header from '../Слой-600-копия.png'
+import header from '../bg.png'
 
 class Header extends Component {
-  constructor(props) {
-    super(props)
-
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(selectorFiles) {
-    this.props.sendHeaderImage(selectorFiles)
-  }
-
   render() {
     return (
       <div className="header-container">
-        <img src={this.props.src !== undefined || this.props.src !== '' ? this.props.src : header} alt="header" />
-        <input type="file" name="header" accept="image/*" onChange={e => this.handleChange(e.target.files)} />
+        <img src={header} alt="header" />
       </div>
     )
   }
--- routs/verifyToken.js
+++ routs/verifyToken.js
@@ -5,9 +5,7 @@ module.exports = (req, res, next) => {
   if(!token) return res.status(401).send('Access Denied')
   
   try {
-    console.log('here1')
     const verified = jwt.verify(token, process.env.TOKEN_SECRET)
-    console.log('here2')
     req.user = verified
     next()
   } catch (err) {
